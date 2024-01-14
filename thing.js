d7b9089a074757272759563be8756b963259936f
Nathan Coleman <92738130+NTC21@users.noreply.github.com>
2024-01-14 17:58:27 -0500

    reusing old divs and adding style

--- thing.js
+++ thing.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 function getComputerChoice(){
     let rand = Math.random()
     rand = Math.floor(rand* 3)
@@ -28,65 +23,75 @@ let btns = document.querySelectorAll('.btn');
 
 btns.forEach((btn) => {
     btn.addEventListener('click', () => {
+
+
         let playerSelection2 = btn.textContent;
         console.log("player selected", playerSelection2);
 
         let computerSelection = getComputerChoice();
         console.log("computer selected", computerSelection);
 
-    /* WIN for player*/
-    if ((playerSelection2 === "scissors" && computerSelection === "paper")||
-        (playerSelection2 === "paper" && computerSelection === "rock")||
-        (playerSelection2 === "rock" && computerSelection === "scissors")){
-        playerscore +=0;
-        console.log(`You Win! ${playerSelection2} beats ${computerSelection}`);
-        const content1 = document.createElement('div');
-        content1.textContent = `You Win This Round! ${playerSelection2} beats ${computerSelection}`;
-        container.appendChild(content1);
+// create a div for the place where we tell who won the round 
+
+        let computerscore1 = document.querySelector('#computerscore1');
+        let playerscore1 = document.querySelector('#playerscore1');
+
+        playerscore1.textContent = `${playerscore}`;
+        computerscore1.textContent = `${computerscore}`;
 
 
-    }
+        /* WIN for player*/
+        if ((playerSelection2 === "scissors" && computerSelection === "paper")||
+            (playerSelection2 === "paper" && computerSelection === "rock")||
+            (playerSelection2 === "rock" && computerSelection === "scissors")){
+            playerscore +=1;
+            playerscore1.textContent = `${playerscore}`;
+            console.log(`You Win! ${playerSelection2} beats ${computerSelection}`);
+            container.textContent = `You Win This Round! ${playerSelection2} beats ${computerSelection}`;
 
-    /* WIN for computer*/
-    if ((playerSelection2 === "scissors" && computerSelection === "rock")||
-        (playerSelection2 === "paper" && computerSelection === "scissors")||
-        (playerSelection2 === "rock" && computerSelection === "paper")){
-        computerscore+=1;
-        console.log(`You Lose! ${computerSelection} beats ${playerSelection2}`);
-        const content2 = document.createElement('div');
-        content2.textContent = `You Lose This Round! ${computerSelection} beats ${playerSelection2}`;
-        container.appendChild(content2);
 
-    }
-    
-    /* Tie */
-    if ((playerSelection2 === "scissors" && computerSelection === "scissors")||
-        (playerSelection2 === "paper" && computerSelection === "paper")||
-        (playerSelection2 === "rock" && computerSelection === "rock")){
-        console.log(`Tie. Redo`);
-        const content3 = document.createElement('div');
-        content3.textContent = `Tie this Round. Redo`;
-        container.appendChild(content3);
+        }
 
-    }
+        /* WIN for computer*/
+        if ((playerSelection2 === "scissors" && computerSelection === "rock")||
+            (playerSelection2 === "paper" && computerSelection === "scissors")||
+            (playerSelection2 === "rock" && computerSelection === "paper")){
+            computerscore+=1;
+            computerscore1.textContent = `${computerscore}`;
 
-    if (playerscore === 5){
-        console.log(`PLAYER WINS GAME`);
-        const content4 = document.createElement('h2');
-        content4.textContent = `PLAYER WINS GAME`;
-        container.appendChild(content4);
-        playerscore = 0;
-        computerscore = 0;
-    }
+            console.log(`You Lose! ${computerSelection} beats ${playerSelection2}`);
+            container.textContent = `You Lose This Round! ${computerSelection} beats ${playerSelection2}`;
 
-    if (computerscore === 5){
-        console.log(`COMPUTER WINS GAME`);
-        const content5 = document.createElement('h2');
-        content5.textContent = `COMPUTER WINS GAME`;
-        container.appendChild(content5);
-        computerscore = 0;
-        playerscore = 0;
-    }
+        }
+        
+        /* Tie */
+        if ((playerSelection2 === "scissors" && computerSelection === "scissors")||
+            (playerSelection2 === "paper" && computerSelection === "paper")||
+            (playerSelection2 === "rock" && computerSelection === "rock")){
+            console.log(`Tie. Redo`);
+            container.textContent = `Tie this Round. Redo`;
+
+        }
+
+        if (playerscore === 5){
+            console.log(`PLAYER WINS GAME`);
+            const content4 = document.createElement('h2');
+            content4.textContent = `PLAYER WINS GAME`;
+            container.appendChild(content4);
+            playerscore = 0;
+            computerscore = 0;
+
+        }
+
+        if (computerscore === 5){
+            console.log(`COMPUTER WINS GAME`);
+            const content5 = document.createElement('h2');
+            content5.textContent = `COMPUTER WINS GAME`;
+            container.appendChild(content5);
+            computerscore = 0;
+            playerscore = 0;
+
+        }
 
     });
 });
@@ -96,56 +101,3 @@ btns.forEach((btn) => {
 
 
 
-
-// function singleround(playerSelection2, computerSelection){
-//     // let playerSelection2 = playerSelection.toLowerCase();
-
-
-
-//     /* WIN for player*/
-//     if ((playerSelection2 === "scissors" && computerSelection === "paper")||(playerSelection2 === "paper" && computerSelection === "rock")||(playerSelection2 === "rock" && computerSelection === "scissors")){
-//         playerwin +=1
-//         return `You Win! ${playerSelection} beats ${computerSelection}`;
-//     }
-
-//     /* WIN for computer*/
-//     if ((playerSelection2 === "scissors" && computerSelection === "rock")||(playerSelection2 === "paper" && computerSelection === "scissors")||(playerSelection2 === "rock" && computerSelection === "paper")){
-//         computerwin+=1
-//         return `You Lose! ${computerSelection} beats ${playerSelection}`;
-//     }
-    
-//     /* Tie */
-//     if ((playerSelection2 === "scissors" && computerSelection === "scissors")||(playerSelection2 === "paper" && computerSelection === "paper")||(playerSelection2 === "rock" && computerSelection === "rock")){
-//         tie +=1;
-//         return `Tie. Redo`;
-//     }
-
-
-// }
-
-
-// function game(){
-//     for(let i = 0; i <5; i++){
-//         let userinput = prompt();
-//         console.log(singleround(userinput, getComputerChoice()));
-//         console.log(`playerscore = ${playerwin}, computerscore = ${computerwin}`)
-
-//         if (tie!=0){
-//             i-=1;
-//             tie-=1;
-//         }
-
-//     }
-    
-//     if (playerwin>computerwin) {
-//         console.log(`player wins`);
-//     }
-//     else {
-//         console.log(`computer wins`);
-//     }
-
-
-//     playerwin = 0;
-//     computerwin = 0;
-//     tie = 0;
-// }
